2b69c436f5242e9c678fb7093e4eb2cbd08c8cee
Gidiiketer1 <gideonkipketer789@gmail.com>
2026-01-15 17:03:15 +0300

    Fix log_queries to include datetime logging

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,11 +1,14 @@
 import sqlite3
 import functools
+from datetime import datetime
+
 
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        query = kwargs.get("query") or (args[0] if args else None)
-        print(f"[LOG] Executing query: {query}")
+        query = kwargs.get("query") or (args[0] if args else "")
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"[{timestamp}] Executing query: {query}")
         return func(*args, **kwargs)
     return wrapper
 
